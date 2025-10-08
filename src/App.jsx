4ec7e50fb40d4b03edfc3b6e9743a67021ae667a
Adam Tsai <adam780930@gmail.com>
2025-10-07 22:09:12 -0700

    added support to capture user input

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,26 @@ function App() {
     });
   }
 
+  function handleAddTask(taskData) {
+    setTasksState((prevState) => {
+      const newTask = {
+        ...taskData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
+  console.log(tasksState);
+
   let content;
 
   if (tasksState.selectedTaskId === null) {
-    content = <NewTask />;
+    content = <NewTask onAdd={handleAddTask} />;
   } else if (tasksState.selectedTaskId === undefined) {
     content = <NoTaskSelected onStartAddTask={handleStartAddTask} />;
   }
