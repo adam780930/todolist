807860fa0182d3aa78fb21c5f72b72ff607f2efa
Adam <100063577+adam780930@users.noreply.github.com>
2025-10-08 22:48:48 -0700

    added new component for viewing saved tasks (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import { useState } from "react";
 import NewTask from "./components/NewTask.jsx";
 import NoTaskSelected from "./components/NoTaskSelected.jsx";
 import TaskSidebar from "./components/TaskSidebar.jsx";
+import SelectedTask from "./components/SelectedTask.jsx";
 
 function App() {
   const [tasksState, setTasksState] = useState({
     selectedTaskId: undefined,
     tasks: [],
   });
+
+  function handleSelectTask(id) {
+    setTasksState((prevState) => {
+      return {
+        ...prevState,
+        selectedTaskId: id,
+      };
+    });
+  }
 
   function handleStartAddTask() {
     setTasksState((prevState) => {
@@ -44,7 +54,11 @@ function App() {
     });
   }
 
-  let content;
+  const selectedTask = tasksState.tasks.find(
+    (task) => task.id === tasksState.selectedTaskId
+  );
+
+  let content = <SelectedTask task={selectedTask} />;
 
   if (tasksState.selectedTaskId === null) {
     content = <NewTask onAdd={handleAddTask} onCancel={handleCancelAddTask} />;
@@ -57,6 +71,7 @@ function App() {
       <TaskSidebar
         onStartAddTask={handleStartAddTask}
         tasks={tasksState.tasks}
+        onSelectTask={handleSelectTask}
       />
       {content}
     </main>
